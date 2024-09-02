3188a4d32224d4ce911f9cefb3175e9c5924d034
Kuvshinov Alexey <poqe64@gmail.com>
2024-09-02 11:01:56 +0300

    Start adding actual functionality

---
+++ lobby_statistics.go
@@ -0,0 +1,55 @@
+package osu_lobby_inspector
+
+import (
+	"github.com/SnakeTwix/osu-lobby-inspector/internal/api"
+	"log"
+	"time"
+)
+
+type StatisticFetcher struct {
+	clientId     int
+	clientSecret string
+	client       api.Client
+}
+
+type LobbyStatistic struct {
+	LobbyId       int       `json:"lobby_id"`
+	CreatedBy     *User     `json:"created_by"`
+	Users         []User    `json:"users"`
+	CreationDate  time.Time `json:"creation_date"`
+	DisbandedDate time.Time `json:"disbanded_date"`
+
+	// TODO Picked maps
+	// TODO Mapper pick count
+}
+
+func NewStatisticFetcher(clientId int, clientSecret string) (*StatisticFetcher, error) {
+	client := api.New(clientId, clientSecret)
+	err := client.GetToken()
+	if err != nil {
+		return nil, err
+	}
+
+	return &StatisticFetcher{
+		clientId:     clientId,
+		clientSecret: clientSecret,
+		client:       client,
+	}, nil
+
+}
+
+func (f *StatisticFetcher) GetLobbyStatistic(lobbyId int) (LobbyStatistic, error) {
+
+	matchData, err := f.client.GetFullMatch(lobbyId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lobbyStats := LobbyStatistic{
+		LobbyId:       lobbyId,
+		CreationDate:  matchData.Match.StartTime,
+		DisbandedDate: matchData.Match.EndTime,
+	}
+
+	return lobbyStats, nil
+}
---
+++ user.go
@@ -0,0 +1,43 @@
+package osu_lobby_inspector
+
+import "time"
+
+type User struct {
+	UserId           int
+	Username         string
+	JoinCount        int
+	TotalTimeInLobby time.Duration
+	HostCount        int
+	TotalHits        int
+	TotalMisses      int
+	MaxCombo         int
+	MaxScore         int
+
+	// TODO Picked maps
+	// TODO Map Places
+	// TODO Played Maps
+	// TODO Mods played statistic
+}
+
+func (f *StatisticFetcher) GetUser(userId int) (User, error) {
+	//fetchedUser, err := f.client
+
+	user := User{}
+
+	return user, nil
+}
+
+func (f *StatisticFetcher) GetUsers(userIds []int) ([]User, error) {
+	var users []User
+
+	for _, userId := range userIds {
+		user, err := f.GetUser(userId)
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
